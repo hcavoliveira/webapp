8d703f2e3234cb92b7eff8ef8889b2d2931349b1
hcavoliveira <144498938+hcavoliveira@users.noreply.github.com>
2026-03-08 00:26:25 -0300

    Updating app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import pandas as pd
 import scipy.stats
 import streamlit as st
 import time
+
+if 'experiment_no' not in st.session_state:
+    st.session_state['experiment_no'] = 0
+
+if 'df_experiment_results' not in st.session_state:
+    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iterations', 'mean'])
 
 st.header('Jogando uma moeda')
 
@@ -28,6 +35,15 @@ start_button = st.button('Executar')
 
 if start_button:
     st.write(f'Executando o experimento de {number_of_trials} tentativas.')
+    st.session_state['experiment_no'] += 1
     mean = toss_coin(number_of_trials)
+    st.session_state['df_experiment_results'] = pd.concat([
+        st.session_state['df_experiment_results'],
+        pd.DataFrame(data=[[st.session_state['experiment_no'], number_of_trials, mean]], columns=['no', 'iterations', 'mean'])
 
-#st.write('Ainda não é um aplicativo funcional. Em construção.')
+    ],
+    axis=0)
+    st.session_state['df_experiment_results'] = \
+        st.sessions_state['df_experiment_results'].reset_index(drop=True)
+
+st.write(st.sessions_state['df_experiment_results')
